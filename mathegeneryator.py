2005082c1bfd4844afe70a4c46fb7a3cb380ac92
Dariuspetrus <121445323+Dariuspetrus@users.noreply.github.com>
2022-12-28 14:06:10 +0100

    Update mathegeneryator.py
    test

--- mathegeneryator.py
+++ mathegeneryator.py
@@ -1,3 +1,22 @@
-print('Hello mathegenerator')
-print('test')
-print('test')
+#Import Module
+import random
+import tkinter as tk
+from tkinter import ttk
+
+#generate window and optics
+
+#Mainframe
+window= tk.Tk()
+window.title ("Rechenaufgabe:")
+
+# create the problem label
+problem_label = tk.Label(text="Rechenaufgabe:", font=("Arial", 24))
+problem_label.pack()
+
+# create the answer entry field
+answer_entry = tk.Entry(font=("Arial", 24))
+answer_entry.pack()
+
+
+# run the main loop
+window.mainloop()
