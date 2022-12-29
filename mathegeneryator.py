a21e332ca9e03de3f0421c0ffab06e51e3077ed6
Dariuspetrus <121445323+Dariuspetrus@users.noreply.github.com>
2022-12-29 15:34:30 +0100

    Update mathegeneryator.py

--- mathegeneryator.py
+++ mathegeneryator.py
@@ -1,7 +1,7 @@
 #Import Module
 import random
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 
 
 #generate window and optics
@@ -18,27 +18,61 @@ problem_label.pack()
 answer_entry = tk.Entry(font=("Arial", 24))
 answer_entry.pack()
 
+
 # Create a checkbox
 
 checkbox_addition = tk.IntVar()
 checkbox = tk.Checkbutton(window, text="+", variable=checkbox_addition, font=("Arial", 12))
+checkbox_addition.set(True)
 checkbox.pack()
 
 checkbox_subtraktion = tk.IntVar()
 checkbox = tk.Checkbutton(window, text="-", variable=checkbox_subtraktion, font=("Arial", 12))
+checkbox_subtraktion.set(True)
 checkbox.pack()
 
 checkbox_multiplikation = tk.IntVar()
 checkbox = tk.Checkbutton(window, text="x", variable=checkbox_multiplikation, font=("Arial", 12))
+checkbox_multiplikation.set(True)
 checkbox.pack()
 
 checkbox_division = tk.IntVar()
 checkbox = tk.Checkbutton(window, text=":", variable=checkbox_division, font=("Arial", 12))
+checkbox_division.set(True)
 checkbox.pack()
 
-print (checkbox_division)
-print (checkbox_addition)
+def update_operations(any):
+   
+    # Enable or disable the operations based on the checkbox values
+    if checkbox_addition.get() == 1 :
+        additionrechnen = True
+    else:
+        additionrechnen = False
 
+    if checkbox_subtraktion.get() == 1:
+        subtraktionrechnen = True
+    else:
+        subtraktionrechnen = False
+
+    if checkbox_multiplikation.get() == 1:
+        multiplikationrechnen = True
+    else:
+        multiplikationrechnen = False
+
+    if checkbox_division.get() == 1:
+       divisionrechnen = True
+    else:
+        divisionrechnen = False
+
+
+
+# Call the update_operations function whenever a checkbox is clicked
+checkbox_addition.trace("w", update_operations)
+checkbox_subtraktion.trace("w", update_operations)
+checkbox_multiplikation.trace("w", update_operations)
+checkbox_division.trace("w", update_operations)
+
+#print(update_operations)
 # Create Numberspace
 
 
