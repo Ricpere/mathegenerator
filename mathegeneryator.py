e712e0f028bbae4ece9cd130e035a43a971f2a89
Dariuspetrus <121445323+Dariuspetrus@users.noreply.github.com>
2022-12-29 11:43:55 +0100

    Test

--- mathegeneryator.py
+++ mathegeneryator.py
@@ -3,6 +3,8 @@ import random
 import tkinter as tk
 from tkinter import ttk
 
+
+
 #generate window and optics
 
 #Mainframe
@@ -14,9 +16,18 @@ problem_label = tk.Label(text="Rechenaufgabe:", font=("Arial", 24))
 problem_label.pack()
 
 # create the answer entry field
-answer_entry = tk.Entry(font=("Consola", 24))
+answer_entry = tk.Entry(font=("Arial", 24))
 answer_entry.pack()
+
+#create Checkboxes
+
+# Create Numberspace
 
 
 # run the main loop
 window.mainloop()
+
+#Classes
+
+class Problem(window):
+    defzahl=0 #placeholder
