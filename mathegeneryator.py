3a1cfc1c51d76037245dcfc8c765ca8aa8f728b8
Dariuspetrus <121445323+Dariuspetrus@users.noreply.github.com>
2023-02-11 14:34:57 +0100

    Update mathegeneryator.py

--- mathegeneryator.py
+++ mathegeneryator.py
@@ -11,82 +11,82 @@ V0.5 Bugfixes gemäss Testsprint 1 (Anpassung der Variablen auf Global)
 """
 
 
-
-#Import Module
+# Import Module
 import random
 import tkinter as tk
 from tkinter import messagebox
 
-#Define Variables
+# Define Variables
 additionrechnen = True
 subtraktionrechnen = True
 multiplikationrechnen = True
 divisionrechnen = True
 result = int(0)
 rechnung = int(0)
-user_answer = False 
-num1= random.randint(1, 20)
-num2= random.randint(1, 20)
+user_answer = False
+num1 = random.randint(1, 20)
+num2 = random.randint(1, 20)
 
-#generate Numbers
+# generate Numbers
+
 
 def generate_number():
     global num1
     global num2
-    num1= random.randint(1, 20)
-    num2= random.randint(1, 20)
-#generate Math Calculation
+    num1 = random.randint(1, 20)
+    num2 = random.randint(1, 20)
+# generate Math Calculation
+
 
 def generate_problem(*args):
 
-    #chose Operations
+    # chose Operations
 
     operations = []
     if additionrechnen == True:
-        operations.append ("addition")
+        operations.append("addition")
     if subtraktionrechnen == True:
-        operations.append ("subtraktion")
+        operations.append("subtraktion")
     if multiplikationrechnen == True:
-        operations.append ("multiplikation")
+        operations.append("multiplikation")
     if divisionrechnen == True:
-        operations.append ("division")
+        operations.append("division")
 
     global result
     global rechnung
-    
 
     chosen_operation = random.choice(operations)
     if chosen_operation == "addition":
         result = num1 + num2
-        rechnung = "%i + %i" %(num1, num2)
+        rechnung = "%i + %i" % (num1, num2)
     if chosen_operation == "subtraktion":
         result = num1 - num2
-        rechnung= "%i - %i" %(num1, num2)
+        rechnung = "%i - %i" % (num1, num2)
     if chosen_operation == "multiplikation":
         result = num1 * num2
-        rechnung= "%i * %i" %(num1, num2)
+        rechnung = "%i * %i" % (num1, num2)
     if chosen_operation == "division":
-        tempresultat= num1 * num2
+        tempresultat = num1 * num2
         result = tempresultat / num1
-        rechnung= "%i : %i" %(tempresultat, num1)
+        rechnung = "%i : %i" % (tempresultat, num1)
 
 
-#generate Problem
+# generate Problem
 
 generate_problem()
 
 
-#generate window and optics
+# generate window and optics
 
-#Mainframe
-window= tk.Tk()
-window.title ("Kofprechnen macht Spass!")
+# Mainframe
+window = tk.Tk()
+window.title("Kofprechnen macht Spass!")
 
 # create the problem label title
 problem_label_title = tk.Label(text="Rechenaufgabe:", font=("Arial", 24))
 problem_label_title.grid(row=0, column=0)
 
-# create the problem 
+# create the problem
 problem = tk.Label(text=rechnung, font=("Arial", 24))
 problem.grid(row=1, column=0)
 
@@ -94,68 +94,77 @@ problem.grid(row=1, column=0)
 answer_entry = tk.Entry(font=("Arial", 24),)
 answer_entry.grid(row=2, column=0)
 
-#Create Submitbutton and the logic of it
+# Create Submitbutton and the logic of it
+
+
 def check_answer():
     update_operations()
     global rechnung
     global result
     global user_answer
     user_answer = int(answer_entry.get())
-    if  user_answer == result:
-        print ("Rechnung richtig")
-        print ("Input=",user_answer)
-        print ("Resultat=", result)
+    if user_answer == result:
+        print("Rechnung richtig")
+        print("Input=", user_answer)
+        print("Resultat=", result)
         generate_number()
         generate_problem()
-        
+
         problem.config(text=rechnung)
-        
-        
+
         return True
-        
+
     else:
-        print ("Rechnung falsch")
-        print ("Input=", user_answer)
-        print ("Resultat=", result)
+        print("Rechnung falsch")
+        print("Input=", user_answer)
+        print("Resultat=", result)
         return False
-answer_button = tk.Button(text= "Resultat Prüfen", font=("Arial", 12),command= check_answer)
+
+
+answer_button = tk.Button(text="Resultat Prüfen", font=(
+    "Arial", 12), command=check_answer)
 answer_button.grid(row=3, column=0)
 
 
-#frame
+# frame
 frame = tk.Frame(window, borderwidth=0, relief="solid")
 frame.grid(row=4, column=0)
 
 # Create a checkbox
 
 checkbox_addition = tk.IntVar()
-checkbox = tk.Checkbutton(frame, text="+", variable=checkbox_addition, font=("Arial", 12))
+checkbox = tk.Checkbutton(
+    frame, text="+", variable=checkbox_addition, font=("Arial", 12))
 checkbox_addition.set(True)
 checkbox.grid(row=4, column=0)
-#checkbox.pack()
+# checkbox.pack()
 
 checkbox_subtraktion = tk.IntVar()
-checkbox = tk.Checkbutton(frame, text="-", variable=checkbox_subtraktion, font=("Arial", 12))
+checkbox = tk.Checkbutton(
+    frame, text="-", variable=checkbox_subtraktion, font=("Arial", 12))
 checkbox_subtraktion.set(True)
 checkbox.grid(row=4, column=1, padx=5, pady=5)
-#checkbox.pack()
+# checkbox.pack()
 
 checkbox_multiplikation = tk.IntVar()
-checkbox = tk.Checkbutton(frame, text="x", variable=checkbox_multiplikation, font=("Arial", 12),)
+checkbox = tk.Checkbutton(
+    frame, text="x", variable=checkbox_multiplikation, font=("Arial", 12),)
 checkbox_multiplikation.set(True)
 checkbox.grid(row=4, column=2, padx=5, pady=5)
-#checkbox.pack()
+# checkbox.pack()
 
 checkbox_division = tk.IntVar()
-checkbox = tk.Checkbutton(frame, text=":", variable=checkbox_division, font=("Arial", 12))
+checkbox = tk.Checkbutton(
+    frame, text=":", variable=checkbox_division, font=("Arial", 12))
 checkbox_division.set(True)
 checkbox.grid(row=4, column=3, padx=5, pady=5)
-#checkbox.pack()
+# checkbox.pack()
 
 window.columnconfigure(0, pad=60)
 window.columnconfigure(1, pad=60)
 window.columnconfigure(2, pad=60)
 window.columnconfigure(3, pad=60)
+
 
 def update_operations(*args):
 
@@ -164,35 +173,35 @@ def update_operations(*args):
     global divisionrechnen
     global multiplikationrechnen
     # Enable or disable the operations based on the checkbox values
-    if checkbox_addition.get() == 1 :
+    if checkbox_addition.get() == 1:
         additionrechnen = True
-        print ("Additionsrechnen an")
+        print("Additionsrechnen an")
     else:
         additionrechnen = False
-        print ("Additionsrechnen aus")
+        print("Additionsrechnen aus")
 
     if checkbox_subtraktion.get() == 1:
         subtraktionrechnen = True
-        print ("Subtraktionsrechnen an")
+        print("Subtraktionsrechnen an")
     else:
         subtraktionrechnen = False
-        print ("Subtraktionsrechnen aus")
+        print("Subtraktionsrechnen aus")
 
     if checkbox_multiplikation.get() == 1:
         multiplikationrechnen = True
-        print ("Multiplikationsrechnen an")
+        print("Multiplikationsrechnen an")
     else:
         multiplikationrechnen = False
-        print ("Multiplikationsrechnen aus")
+        print("Multiplikationsrechnen aus")
 
     if checkbox_division.get() == 1:
        divisionrechnen = True
-       print ("Divisionsrechnen an")
+       print("Divisionsrechnen an")
     else:
         divisionrechnen = False
-        print (divisionrechnen)
-        print ("Divisionsrechnen aus")
-    
+        print(divisionrechnen)
+        print("Divisionsrechnen aus")
+
 
 # Call the update_operations function whenever a checkbox is clicked
 checkbox_addition.trace("w", update_operations)
@@ -200,7 +209,7 @@ checkbox_subtraktion.trace("w", update_operations)
 checkbox_multiplikation.trace("w", update_operations)
 checkbox_division.trace("w", update_operations)
 
-#print(update_operations)
+# print(update_operations)
 # Create Numberspace
 
 
